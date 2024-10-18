0ae3cce2297f401d5752d3c8cff1acf7d311e148
OwnerOfJK <john.kaller@hotmail.de>
2024-10-18 14:04:21 +0700

    first exercise done

---
+++ exercises/finite_fields/01_sets_and_groups/README.md
@@ -0,0 +1,3 @@
+## Finite Fields
+
+**Finite fields** are a core element of cryptography and crucial when working with zero knowledge proofs. The goal within this chapter is to develop an intuition of the properties of a finite fiels and why they are important.
---
+++ solutions/finite_fields/01_sets_and_groups/01_intro.rs
@@ -0,0 +1,25 @@
+// Sets and groups
+
+// To better understand finite fields, and what fields are in general, we need to start with elements, sets, groups and operations.
+
+// Exercise 1: Implement a simple addition operation on a set of integers.
+
+use std::vec::Vec;
+
+fn main() {
+    // Given a set of integers. Each integer number is an element of the set.
+    let set: Vec<i32> = vec![-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
+
+    // We can run operations any set element.
+    let result = add_operation(set[1], set[2]);
+
+    // print result
+    println!("Result of addition: {}", result);
+}
+
+// TODO: Implement this function
+fn add_operation(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// Learning: A group is the combination of a set and an operation.
